7f809ce96ead4f385fcf5f74c05fcdcaf779cd82
SuryaTeja21 <csuryateja2002@gmail.com>
2022-11-07 11:23:48 +0530

    commit

---
+++ src/main/java/com/sample/maven1/App.java
@@ -0,0 +1,13 @@
+package com.sample.maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Po ra Badcow" );
+    }
+}
